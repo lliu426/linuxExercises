f013f2298173d2901aafa6d5679e0c72b2978a75
Yuchen Wang <ywang2668@wisc.edu>
2025-03-02 00:28:04 -0600

    finish the required code.

--- rm_n.sh
+++ rm_n.sh
@@ -1,1 +1,12 @@
 #!/bin/bash
+if [[ $# -ne 2 ]]; then
+    echo "usage: $0 <dir> <n>" 1>&2
+    exit 1
+fi
+dir=$1
+n=$2
+if [[ ! -d $dir ]]; then
+    echo "Wrong name '$dir'" 1>&2
+    exit 1
+fi
+find "$dir" -type f -size +"${n}c" -exec rm {} \;
