7c2757e3344a968774b69c61d827bdb1526a7135
Lexiang Liu <lliu426@wisc.edu>
2025-03-04 08:53:42 -0600

    Output both sum and average

--- school.sh
+++ school.sh
@@ -1,10 +1,16 @@
 #!/bin/bash
 
-cat Property_Tax_Roll.csv | grep "MADISON SCHOOLS" | cut -d ',' -f 7 | tail -n +2 | awk '
-    BEGIN { sum = 0; count = 0; }
-    {
-        sum += $1;
-        count++;
-    }
-    END { print sum / count }
-'
+cat Property_Tax_Roll.csv | \
+grep "MADISON SCHOOLS" | \
+cut -d ',' -f7 | \
+tail -n +2 | \
+{
+    sum=0
+    count=0
+    while read value; do
+        sum=$((sum + value))
+        count=$((count + 1))
+    done
+    echo "Sum: $sum"
+    echo "Average: $((sum / count))"
+}
