5b29aae9b18db95ad98a9ff2c804f4adefcf1333
Huiyu Li <hli798@public03.stat.wisc.edu>
2025-03-04 22:51:30 -0600

    Add five_dirs.sh and group.csv

--- five_dirs.sh
+++ five_dirs.sh
@@ -1,1 +1,13 @@
 #!/bin/bash
+mkdir five
+for i in {1..5}
+do
+    mkdir five/dir$i
+    for j in {1..4}
+    do
+	for line in $(seq 1 $j); do
+	    echo "$j" >> five/dir$i/file$j
+	done
+    done
+done
+echo "All done. created directory five with five subdirectories and 20 files."
